33eba662e018ed0b95c9ae2186899f9aab10d4ae
Philip Cullen <philipcullen93@gmail.com>
2025-03-03 21:06:30 +0000

    Uploaded weekly task weekday.py

---
+++ weekday.py
@@ -0,0 +1,14 @@
+# this program checks the what day it is
+# it then prints a message depending on whether it is a weekday or a weekend day
+
+# author: Philip Cullen
+
+import datetime
+
+week_days = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+now = datetime.datetime.now()
+day = week_days[now.weekday()]
+if day == week_days[5 : 6] :
+    print("Yay, it is the weekend!")
+else :
+    print("Unfortunately, it is a weekday.")
