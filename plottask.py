1374cd59d8d70274d331fafc7ab84476665eeb21
Philip Cullen <philip.cullen93@gmail.com>
2025-03-31 14:35:50 +0100

    Added line plot

--- plottask.py
+++ plottask.py
@@ -1,11 +1,12 @@
 # write a program called plottask.py that displays:
 # a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2 
-# and a plot of the function  h(x)=x3 in the range 0 to 10, 
+# and a plot of the function  h(x)=x^3 in the range 0 to 10, 
 # on the one set of axes.
 
 
 # Author: Philip Cullen
 
+# Part 1: Histogram
 # start by import numpy and matplotlib.pyplot
 import random
 import numpy as np
@@ -18,10 +19,35 @@ stddev = 2
 values = np.random.normal(mean, stddev, 1000)
 
 # create histogram
-
 plt.hist(values, color = 'red', alpha = 1, edgecolor = 'black')
+
+# add labels and title
 plt.xlabel('Value')
 plt.ylabel('Frequency')
 plt.title("Histogram of 1000 values in a Normal Distribution")
+# display histogram
 plt.show()
+
+# Part 2: Subplot
+# plot the function  h(x)=x^3 in the range 0 to 10
+xpoints = np.array(range(1, 10))
+ypoints = xpoints ** 3
+
+# create the plot
+plt.plot(xpoints, ypoints, label = 'h(x) = x^3')
+
+plt.xlabel("x")
+plt.ylabel("h(x)")
+plt.title("Plot of h(x) = x^3")
+plt.legend()
+
+# display plot
+plt.show()
+
+
+
+
+
+
+
 
